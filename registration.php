a2d0c0af96d3933c91f3e816650d6978c700418b
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-08 13:16:02 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/sug.css">
+    <link rel="stylesheet" href="/css/font-awesome.css">
+    <title>Страхование жизни</title>
+  </head>
+  <body>
+    <div class="container">
+      <a href="/index.php"><img src="img/lock.png"></a>
+      <form class="" action="sugnup.php" method="post">
+        <div class="dws-input">
+          <input type="text" name="username" placeholder="Придумайте логин">
+          <input type="password" name="password-1" placeholder="Придумайте пароль">
+          <input type="password" name="password-2" placeholder="Введите пароль еще раз">
+          <input type="text" name="email" placeholder="Ваш email...">
+        </div>
+        <input class="dws-submit" type="submit" name="submit" value="Регистрация">
+      </form>
+    </div>
+  </body>
+</html>
 <?php
 
 require_once('db.php');
@@ -32,28 +55,4 @@ if (isset($_POST['submit'])) {
     }
   }
 ?>
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/sug.css">
-    <link rel="stylesheet" href="/css/font-awesome.css">
-    <title>Страхование жизни</title>
-  </head>
-  <body>
-    <div class="container">
-      <a href="/index.php"><img src="img/lock.png"></a>
-      <form class="" action="sugnup.php" method="post">
-        <div class="dws-input">
-          <input type="text" name="username" placeholder="Придумайте логин">
-          <input type="password" name="password-1" placeholder="Придумайте пароль">
-          <input type="password" name="password-2" placeholder="Введите пароль еще раз">
-          <input type="text" name="email" placeholder="Ваш email...">
-        </div>
-        <input class="dws-submit" type="submit" name="submit" value="Регистрация">
-      </form>
-    </div>
-  </body>
-</html>
 
