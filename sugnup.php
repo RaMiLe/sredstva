42d7bdc9b894519d7c572947292b97d501e4136f
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-08 18:26:54 +0300

    Update sugnup.php

--- sugnup.php
+++ sugnup.php
@@ -68,35 +68,16 @@ if (isset($_POST['submit'])) {
 		<span>Введите ваши регистрационные данные для входа в ваш личный кабинет. </span>
     </div>
 
-    <div class="dws-input">
+      <a href="/index.php"><img src="img/lock.png"></a>
+      <form class="" action="sugnup.php" method="post">
+        <div class="dws-input">
           <input type="text" name="username" placeholder="Придумайте логин">
           <input type="password" name="password-1" placeholder="Придумайте пароль">
           <input type="password" name="password-2" placeholder="Введите пароль еще раз">
           <input type="text" name="email" placeholder="Ваш email...">
         </div>
         <input class="dws-submit" type="submit" name="submit" value="Регистрация">
+      </form>
     </div>
-
-</form>
-</div>
-<div class="gradient"></div>
-
-<script type="text/javascript">
-$(document).ready(function() {
-	$(".username").focus(function() {
-		$(".user-icon").css("left","-48px");
-	});
-	$(".username").blur(function() {
-		$(".user-icon").css("left","0px");
-	});
-	
-	$(".password").focus(function() {
-		$(".pass-icon").css("left","-48px");
-	});
-	$(".password").blur(function() {
-		$(".pass-icon").css("left","0px");
-	});
-});
-</script>
-</body>
+  </body>
 </html>
