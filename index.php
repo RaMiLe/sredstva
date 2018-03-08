2ecf4352baba04d501f39cc3fad86b81df90173a
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-08 17:35:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,29 +20,70 @@ if (isset($_POST['submit'])) {
   }
  ?>
 
-<!DOCTYPE HTML>
 <html>
-	<head>
-		<meta charset="utf-8">
-    <link rel="stylesheet" href="/css/style.css">
-		<title>Вход</title>
-	</head>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>22 лучших формы входа и регистрации | Vladmaxi.net</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+	<link rel="icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
+</head>
 <body>
-	<div class="box">
-		<form action="index.php" method="POST">
-      <div class="box-input">
-        <label>Логин</label>
-    		<input type="text" name="login">
-      </div>
-      <div class="box-input">
-        <label>Пароль</label>
-    		<input type="password" name="password">
-      </div>
-      <div class="box-submit">
-        <input type="submit" name="submit" value="OK">
-      </div>
-      <a href="sugnup.php">Регистрация</a>
-    </form>
-	</div>
+
+<!-- vladmaxi top bar -->
+    <div class="vladmaxi-top">
+        <a href="http://vladmaxi.net" target="_blank">Главная Vladmaxi.net</a>
+        <span class="right">
+        <a href="http://vladmaxi.net/web-developer/css/22-luchshix-formy-vxoda-i-registracii-na-sajte-v-html-css.html">
+                <strong>Вернуться назад к статье</strong>
+            </a>
+        </span>
+    <div class="clr"></div>
+    </div>
+<!--/ vladmaxi top bar -->
+
+<div id="wrapper">
+    <div class="user-icon"></div>
+    <div class="pass-icon"></div>
+	
+<form name="login-form" class="login-form" action="" method="post">
+
+    <div class="header">
+		<h1>Авторизация</h1>
+		<span>Введите ваши регистрационные данные для входа в ваш личный кабинет. </span>
+    </div>
+
+    <div class="content">
+		<input name="username" type="text" class="input username" value="Логин" onfocus="this.value=''" />
+		<input name="password" type="password" class="input password" value="Пароль" onfocus="this.value=''" />
+    </div>
+
+    <div class="footer">
+		<input type="submit" name="submit" value="ВОЙТИ" class="button" />
+		<input type="submit" name="submit" value="Регистрация" class="register" />
+    </div>
+
+</form>
+</div>
+<div class="gradient"></div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$(".username").focus(function() {
+		$(".user-icon").css("left","-48px");
+	});
+	$(".username").blur(function() {
+		$(".user-icon").css("left","0px");
+	});
+	
+	$(".password").focus(function() {
+		$(".pass-icon").css("left","-48px");
+	});
+	$(".password").blur(function() {
+		$(".pass-icon").css("left","0px");
+	});
+});
+</script>
 </body>
 </html>
