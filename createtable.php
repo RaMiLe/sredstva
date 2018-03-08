43cce2c494a2fb44c362b62036f0094f6f7d9e44
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-08 12:22:26 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,19 +1,18 @@
 <?
-
 try {
-    $conn = new PDO("sqlsrv:server = tcp:deneg.database.windows.net,1433; Database = asus", "den", "Rosbank20");
+     $conn = new PDO("sqlsrv:server = tcp:deneg.database.windows.net,1433; Database = asus", "den", "Rosbank20");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-id INT NOT NULL AUTO_INCREMENT ,
-login  VARCHAR( 20 ) NOT NULL ,
+$sql = "CREATE TABLE users(
+id` INT NOT NULL AUTO_INCREMENT ,
+ login VARCHAR( 20 ) NOT NULL ,
  password VARCHAR( 35 ) NOT NULL ,
  email VARCHAR( 50 ) NOT NULL ,
  reg_date VARCHAR( 32 ) NOT NULL ,
  name_user VARCHAR( 32 ) NOT NULL ,
  lastname VARCHAR( 32 ) NOT NULL ,
-$conn->query($sql);
 PRIMARY KEY ( id )
 );
+$conn->query($sql);
 echo "<h3>Таблица создана.</h3>";
 }
 catch (PDOException $e) {
