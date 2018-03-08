507ab5abe08878f78965e74b8b4e44ee550f1157
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-08 18:09:19 +0300

    Update sugnup.php

--- sugnup.php
+++ sugnup.php
@@ -34,26 +34,72 @@ if (isset($_POST['submit'])) {
   }
 ?>
 
-<!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/sug.css">
-    <link rel="stylesheet" href="/css/font-awesome.css">
-    <title>Страхование жизни</title>
-  </head>
-  <body>
-    <div class="container">
-      <a href="/index.php"><img src="img/lock.png"></a>
-      <form class="" action="sugnup.php" method="post">
-        <div class="dws-input">
-          <input type="text" name="username" placeholder="Придумайте логин">
-          <input type="password" name="password-1" placeholder="Придумайте пароль">
-          <input type="password" name="password-2" placeholder="Введите пароль еще раз">
-          <input type="text" name="email" placeholder="Ваш email...">
-        </div>
-       <input class="dws-submit" type="submit" name="submit" value="Регистрация">
-      </form>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>22 лучших формы входа и регистрации | Vladmaxi.net</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+	<link rel="icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
+</head>
+<body>
+
+<!-- vladmaxi top bar -->
+    <div class="vladmaxi-top">
+        <a href="http://vladmaxi.net" target="_blank">Главная Vladmaxi.net</a>
+        <span class="right">
+        <a href="http://vladmaxi.net/web-developer/css/22-luchshix-formy-vxoda-i-registracii-na-sajte-v-html-css.html">
+                <strong>Вернуться назад к статье</strong>
+            </a>
+        </span>
+    <div class="clr"></div>
     </div>
-  </body>
+<!--/ vladmaxi top bar -->
+
+<div id="wrapper">
+    <div class="user-icon"></div>
+    <div class="pass-icon"></div>
+	
+<form name="login-form" class="login-form" action="" method="post">
+
+    <div class="header">
+		<h1>Авторизация</h1>
+		<span>Введите ваши регистрационные данные для входа в ваш личный кабинет. </span>
+    </div>
+
+    <div class="content">
+		<input name="username" type="text" class="input username" value="Придумайте логин" onfocus="this.value=''" />
+		<input name="password" type="password" class="input password" value="Придумайте пароль пароль" onfocus="this.value=''" />
+        <input name="password2" type="password" class="input password2" value="Подвердите пароль" onfocus="this.value=''" />
+        <input name="email" type="text" class="input email" value="Введите почту" onfocus="this.value=''" />
+    </div>
+
+    <div class="footer">
+		<input type="submit" name="submit" value="ВОЙТИ" class="button" />
+		<a href="sugnup.php">Регистрация</a>
+    </div>
+
+</form>
+</div>
+<div class="gradient"></div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$(".username").focus(function() {
+		$(".user-icon").css("left","-48px");
+	});
+	$(".username").blur(function() {
+		$(".user-icon").css("left","0px");
+	});
+	
+	$(".password").focus(function() {
+		$(".pass-icon").css("left","-48px");
+	});
+	$(".password").blur(function() {
+		$(".pass-icon").css("left","0px");
+	});
+});
+</script>
+</body>
 </html>
