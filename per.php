38c5d1f61cca4574639ad60e0265469aa63e7748
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 22:10:53 +0300

    Update and rename per to per.php

---
+++ per.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Background-image</title>
+</head>
+	<table>
+<body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
+	<div class="content">
+		
+		<input name="surname" type="text" class="input surname" value="Номер карты" onfocus="this.value=''" />
+		<select name="country">
+<option value="">Месяц</option>
+<option value="Russia">01</option>
+<option value="USA">02</option>
+<option value="Germany">03</option>
+<option value="Japan">04</option>
+<option value="China">05</option>
+<option value="Russia">06</option>
+<option value="USA">07</option>
+<option value="Germany">08</option>
+<option value="Japan">09</option>
+<option value="China">10</option>
+<option value="Japan">11</option>
+<option value="China">12</option>
+</select> 
+<select name="country">
+<option value="">Месяц</option>
+<option value="Russia">2018</option>
+<option value="USA">2019</option>
+<option value="Germany">2020</option>
+<option value="Japan">2021</option>
+<option value="China">2022</option>
+</select> 
+		
+		<input name="name" type="text" class="input name" value="Код CVC2/CWC2" onfocus="this.value=''" />
+			
+
+															</select>
+		       <a href="sugnup.php">Перевод</a>
+    </div>
+					   </table>
+</body>
+</html>
