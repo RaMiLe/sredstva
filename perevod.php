80bc16fdbe3e7880714c75eb8a6721ccd16b6f75
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 21:56:20 +0300

    Update perevod.php

--- perevod.php
+++ perevod.php
@@ -6,16 +6,8 @@
 <body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
 	<div class="content">
 		
-		<input name="surname" type="text" class="input surname" value="Введите имя" onfocus="this.value=''" />
 		
-		<input name="name" type="text" class="input name" value="Введите фамилию" onfocus="this.value=''" />
-			
-		<input name="patronymic" type="text" class="patronymic" value="Введите отчество" onfocus="this.value=''" />
-			
-	     <input name="pasport" type="text" class="input pasport" value="Введите серию и номер" onfocus="this.value=''" />
-			
-	    <input name="lastname" type="text" class="input lastname" value=" имя и фамилия  получателя" onfocus="this.value=''" />
-            <input name=noumber" type="text" class="input noumber" value="+7 9XX-XXX-XX-XX" onfocus="this.value=''" />
+	
 		<select name="country">
 <option value="">Выберите страну для перевода</option>
 <option value="Russia">Russia</option>
