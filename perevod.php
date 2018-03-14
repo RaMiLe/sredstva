272229dc00bc1cf1c830d5009ed452bfd066de96
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 14:40:24 +0300

    Update perevod.php

--- perevod.php
+++ perevod.php
@@ -3,20 +3,7 @@
 <title>Background-image</title>
 </head>
 <body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
-	<div class="content">
-	     <input name="pasport" type="text" class="input pasport" value="Введите серию и номер" onfocus="this.value=''" />
-	    <input name="lastname" type="text" class="input lastname" value=" имя и фамилия  получателя" onfocus="this.value=''" />
-	    <input name=noumber" type="text" class="input noumber" value="+7 9XX-XXX-XX-XX" onfocus="this.value=''" />
-		<input name="amount for transfer" type="text" class="input transfer" value="Сумма для перевода" onfocus="this.value=''" />
-		<select name="country">
-<option value="">Валюта</option>
-<option value="Russia">Рубаль</option>
-<option value="USA">Доллар</option>
-<option value="Germany">Euro</option>
-
-</select>
-    </div>
-
+	
 </body>
 </html>
 
