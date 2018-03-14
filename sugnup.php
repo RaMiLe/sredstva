1ddf767a63eee1e911db96419c587bca1b8273ce
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 13:02:32 +0300

    Update sugnup.php

--- sugnup.php
+++ sugnup.php
@@ -37,7 +37,7 @@ if (isset($_POST['submit'])) {
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>22 лучших формы входа и регистрации | Vladmaxi.net</title>
+	<title>Регистрация</title>
 	<link href="css/style.css" rel="stylesheet" type="text/css" />
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
 	<link rel="icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
