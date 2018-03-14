5dcd8c62501511d42f5474059d20c244547767ed
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 22:24:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 
-	<Title>Денежный перевод</Title>				
-<html>
+	<html>
 <head>
-<title>Background-image</title>
+<Title>Registration Form</Title>
+<style type="text/css">
+body { background-color:
+#fff; border-top: solid 10px #000;
+color: #333; font-size: .85em;
+margin: 20; padding: 20;
+font-family: "Segoe UI",
+Verdana, Helvetica, Sans-Serif;
+}
+h1, h2, h3,{ color: #000;
+margin-bottom: 0; padding-bottom: 0; }
+h1 { font-size: 2em; }
+h2 { font-size: 1.75em; }
+h3 { font-size: 1.2em; }
+table { margin-top: 0.75em; }
+th { font-size: 1.2em;
+text-align: left; border: none; padding-left: 0; }
+td { padding: 0.25em 2em 0.25em 0em;
+border: 0 none; }
+</style>
 </head>
-	<table>
-<body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
-	<div class="content">
 		
 		
 	
