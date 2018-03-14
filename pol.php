c1536b9606476c78c61686cf284d0486d7deb791
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 22:44:36 +0300

    Update pol.php

--- pol.php
+++ pol.php
@@ -1,1 +1,20 @@
+<html>
+	<Title>Номер карты</Title>
+	<legend>Номер карты получателя</legend>
+	
+<head>
+<title>Background-image</title>
+</head>
+	<table>
+<body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
+	<div class="content">
+		
+		<input name="surname" type="text" class="input surname" value="Номер карты" onfocus="this.value=''" />
+		
 
+															</select>
+		       <a href="sugnup.php">Перевод</a>
+    </div>
+					   </table>
+</body>
+</html>
