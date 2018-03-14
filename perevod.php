9956dc474b2d1f3880a298d049676c8784c64f49
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 14:33:27 +0300

    Update perevod.php

--- perevod.php
+++ perevod.php
@@ -1,7 +1,74 @@
 <html>
 <head>
-<title>Background-image</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Регистрация</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+	<link rel="icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
 </head>
-<body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
+<body background="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg">
+</body>
+
+
+<!-- vladmaxi top bar -->
+    <div class="vladmaxi-top">
+        <a href="http://vladmaxi.net" target="_blank">Главная Vladmaxi.net</a>
+        <span class="right">
+        <a href="http://vladmaxi.net/web-developer/css/22-luchshix-formy-vxoda-i-registracii-na-sajte-v-html-css.html">
+                <strong>Вернуться назад к статье</strong>
+            </a>
+        </span>
+    <div class="clr"></div>
+    </div>
+<!--/ vladmaxi top bar -->
+
+<div id="wrapper">
+    <div class="user-icon"></div>
+    <div class="pass-icon"></div>
+	
+<form name="login-form" class="login-form" action="" method="post">
+
+    <div class="header">
+		<h1>Авторизация</h1>
+		<span>Введите ваши регистрационные данные для входа в ваш личный кабинет. </span>
+    </div>
+
+    <div class="content">
+	     <input name="pasport" type="text" class="input pasport" value="Введите серию и номер" onfocus="this.value=''" />
+	    <input name="lastname" type="text" class="input lastname" value=" имя и фамилия  получателя" onfocus="this.value=''" />
+	    <input name=noumber" type="text" class="input noumber" value="+7 9XX-XXX-XX-XX" onfocus="this.value=''" />
+		<input name="amount for transfer" type="text" class="input transfer" value="Сумма для перевода" onfocus="this.value=''" />
+		<input name="password-1" type="password" class="input password" value="Пароль" onfocus="this.value=''" />
+	    <input name="password-2" type="password" class="input password" value="Подвердите пароль" onfocus="this.value=''" />
+	    <input name="email" type="text" class="input email" value="email" onfocus="this.value=''" />
+    </div>
+
+    <div class="footer">
+		
+		<input class="dws-submit" type="submit" name="submit" value="Регистрация">
+    </div>
+
+</form>
+</div>
+<div class="gradient"></div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$(".username").focus(function() {
+		$(".user-icon").css("left","-48px");
+	});
+	$(".username").blur(function() {
+		$(".user-icon").css("left","0px");
+	});
+	
+	$(".password").focus(function() {
+		$(".pass-icon").css("left","-48px");
+	});
+	$(".password").blur(function() {
+		$(".pass-icon").css("left","0px");
+	});
+});
+</script>
 </body>
 </html>
