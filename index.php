f87cd642cf98e37383aa4598cc530b866651b008
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 22:00:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-
+<html>
+<head>
+<title>Background-image</title>
+</head>
+	<table>
+<body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
+	<div class="content">
+		
+		<input name="surname" type="text" class="input surname" value="Введите имя" onfocus="this.value=''" />
+		
+		<input name="name" type="text" class="input name" value="Введите фамилию" onfocus="this.value=''" />
+			
+<input name="patronymic" type="text" class="patronymic" value="Введите отчество" onfocus="this.value=''" />
+		 <input name=noumber" type="text" class="input noumber" value="+7 9XX-XXX-XX-XX" onfocus="this.value=''" />
+															</select>
+		       <a href="sugnup.php">Перевод</a>
+    </div>
+					   </table>
+</body>
+</html>
