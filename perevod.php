ee133136b7f8e1702346f29175f713a105dc88ef
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 22:15:11 +0300

    Update perevod.php

--- perevod.php
+++ perevod.php
@@ -6,28 +6,15 @@
 <body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
 	<div class="content">
 		
+		<input name="surname" type="text" class="input surname" value="Введите имя" onfocus="this.value=''" />
 		
-	
-		<select name="country">
-<option value="">Выберите страну для перевода</option>
-<option value="Russia">Russia</option>
-<option value="USA">USA</option>
-<option value="Germany">Germany</option>
-<option value="Japan">Japan</option>
-<option value="China">China</option>
-</select>												   
-														   
-		<input name="amount for transfer" type="text" class="input transfer" value="Сумма для перевода" onfocus="this.value=''" />
-		<select name="country">
-<option value="">Валюта</option>
-<option value="Russia">Рубаль</option>
-<option value="USA">Доллар</option>
-<option value="Germany">Euro</option>
-
-</select>
-		       <a href="sugnup.php">Перевод</a>
+		<input name="name" type="text" class="input name" value="Введите фамилию" onfocus="this.value=''" />
+			
+<input name="patronymic" type="text" class="patronymic" value="Введите отчество" onfocus="this.value=''" />
+		 <input name=noumber" type="text" class="input noumber" value="+7 9XX-XXX-XX-XX" onfocus="this.value=''" />
+															</select>
+		       <a href="per.php">Перевод</a>
     </div>
-
 					   </table>
 </body>
 </html>
