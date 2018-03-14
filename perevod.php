e8a7704f688e77566b412f0d55ff1147cca407af
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 21:48:19 +0300

    Update perevod.php

--- perevod.php
+++ perevod.php
@@ -16,7 +16,15 @@
 			
 	    <input name="lastname" type="text" class="input lastname" value=" имя и фамилия  получателя" onfocus="this.value=''" />
             <input name=noumber" type="text" class="input noumber" value="+7 9XX-XXX-XX-XX" onfocus="this.value=''" />
-														   </td>
+		<select name="country">
+<option value="">Выберите страну для перевода</option>
+<option value="Russia">Russia</option>
+<option value="USA">USA</option>
+<option value="Germany">Germany</option>
+<option value="Japan">Japan</option>
+<option value="China">China</option>
+</select>												   
+														   
 		<input name="amount for transfer" type="text" class="input transfer" value="Сумма для перевода" onfocus="this.value=''" />
 		<select name="country">
 <option value="">Валюта</option>
