f2d5cd87166f401a5650dc0e40cc1b8803b061ce
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 15:33:55 +0300

    Update perevod.php

--- perevod.php
+++ perevod.php
@@ -5,9 +5,13 @@
 	<table>
 <body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
 	<div class="content">
+		<td>
 		<input name="surname" type="text" class="input surname" value="Введите имя" onfocus="this.value=''" />
+		</td>
 		<input name="name" type="text" class="input name" value="Введите фамилию" onfocus="this.value=''" />
+		<td>
 		<input name="patronymic" type="text" class="patronymic" value="Введите отчество" onfocus="this.value=''" />
+			</td>
 	     <input name="pasport" type="text" class="input pasport" value="Введите серию и номер" onfocus="this.value=''" />
 	    <input name="lastname" type="text" class="input lastname" value=" имя и фамилия  получателя" onfocus="this.value=''" />
 	    <input name=noumber" type="text" class="input noumber" value="+7 9XX-XXX-XX-XX" onfocus="this.value=''" />
