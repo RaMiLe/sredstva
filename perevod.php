f1f0a080ab49acd1d5ea6ecc951aa188d622f428
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 14:22:28 +0300

    Update perevod.php

--- perevod.php
+++ perevod.php
@@ -2,6 +2,6 @@
 <head>
 <title>Background-image</title>
 </head>
-<body background="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
+<body background-size="https://ic.pics.livejournal.com/irek_murtazin/10510480/1151738/1151738_600.jpg ">
 </body>
 </html>
