0a2c8c74c52401d980c1a480afe5010d35e77a27
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-14 15:35:25 +0300

    Update perevod.php

--- perevod.php
+++ perevod.php
@@ -8,13 +8,21 @@
 		<td>
 		<input name="surname" type="text" class="input surname" value="Введите имя" onfocus="this.value=''" />
 		</td>
+		<td>
 		<input name="name" type="text" class="input name" value="Введите фамилию" onfocus="this.value=''" />
+			</td>
 		<td>
 		<input name="patronymic" type="text" class="patronymic" value="Введите отчество" onfocus="this.value=''" />
 			</td>
+		<td>
 	     <input name="pasport" type="text" class="input pasport" value="Введите серию и номер" onfocus="this.value=''" />
+			</td>
+		<td>
 	    <input name="lastname" type="text" class="input lastname" value=" имя и фамилия  получателя" onfocus="this.value=''" />
+			</td>
+		<td>
 	    <input name=noumber" type="text" class="input noumber" value="+7 9XX-XXX-XX-XX" onfocus="this.value=''" />
+														   </td>
 		<input name="amount for transfer" type="text" class="input transfer" value="Сумма для перевода" onfocus="this.value=''" />
 		<select name="country">
 <option value="">Валюта</option>
