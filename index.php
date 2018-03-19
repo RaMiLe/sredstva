1cb27b6802f820c322a04a7e7d9ea844b5077b27
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-20 00:20:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,74 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WW62942');</script>
+    <!-- End Google Tag Manager -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="pragma" content="no-cache" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/btstrp.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="images/mobile/icons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/mobile/icons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/mobile/icons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/mobile/icons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/mobile/icons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/mobile/icons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/mobile/icons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/mobile/icons/apple-touch-icon-152x152.png">
+    <!--<link rel="icon" type="image/png" href="images/mobile/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="images/mobile/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="images/mobile/icons/favicon-16x16.png" sizes="16x16">-->
+    <link rel="manifest" href="images/mobile/icons/manifest.json">
+    <link rel="mask-icon" href="images/mobile/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="images/mobile/icons/mstile-144x144.png">
+    <meta name="msapplication-config" content="images/mobile/icons/browserconfig.xml">
+    <meta name="theme-color" content="#e4032e">
+
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="«Золотая Корона» - Денежные переводы">
+    <meta property="og:title" content="«Золотая Корона» - Денежные переводы">
+    <meta property="og:description" content="Воспользуйтесь моей ссылкой и отправляйте денежные переводы на специальных условиях! Получите скидку на комиссию или курс конвертации!">
+    <meta property="og:locale" content="ru_RU">
+
+
+    <title>Денежные переводы</title>
+
+    <script type="text/javascript" src="js/metrics.js"></script>
+    <script type="text/javascript" src="https://www.googleadservices.com/pagead/conversion_async.js" charset="utf-8"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.mask.min.js"></script>
+    <script type="text/javascript" src="traplication_b2839/traplication_b2839.nocache.js"></script>
+</head>
+
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MGM3QPL"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<iframe src="javascript:''" id="__gwt_historyFrame" tabIndex='-1' style="position:absolute;width:0;height:0;border:0"></iframe>
+
+<noscript>
+    <div style="width: 22em; position: absolute; left: 50%; margin-left: -11em; color: red; background-color: white; border: 1px solid red; padding: 4px; font-family: sans-serif">
+        Your web browser must have JavaScript enabled
+        in order for this application to display correctly.
+    </div>
+</noscript>
+</body>
+</html>
 
