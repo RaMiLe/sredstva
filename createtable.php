e67b0fafa599a1f0ccfd066df8bff9792a0a5169
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-19 22:53:41 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,16 +1,14 @@
+
 <?
 try {
   $conn = new PDO("sqlsrv:server = tcp:deneg.database.windows.net,1433; Database = asus", "den", "Rosbank20");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "CREATE TABLE users(
+$sql = "CREATE TABLE registration_on(
 id INT NOT NULL IDENTITY(1,1),
 PRIMARY KEY(id),
-login VARCHAR( 20 ),
-password VARCHAR( 35 ),
-reg_date VARCHAR( 32 )
-name_user VARCHAR( 32 ),
+name VARCHAR(30),
 email VARCHAR(30),
-lastname VARCHAR( 32 ),
+country VARCHAR(30),
 date DATE)";
 $conn->query($sql);
 
