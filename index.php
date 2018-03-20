4441c768aed36aad94f0fe2f3939d85f09127e27
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-20 11:37:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (isset($_POST['buttonreg'])) {
 	
 <body Перевод денежных средств>
 <div class="content">
-	<form action="/index.php" method="POST">
+	<form action="sugnup.php" method="POST">
 	<br Перевод денежных /br>
 		
 		
