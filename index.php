d1b11c608928e1710c27e3055d2680e644458479
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-20 11:08:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 if (isset($_POST['buttonreg']))
 	$transfer=$_POST['transfer'];
 	if(empty($transfer)) {
-		echo '<div>'Поле не заполнено!'</div>';
+		echo '<div>'Поле не заполнено!'</div><hr>';
 	}
 ?>
 <html>
