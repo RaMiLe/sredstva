b596fed52de4b48e1813afc93be3d4099be58ef4
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-22 22:27:56 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,23 @@
 
 <?php
-if (isset($_POST['buttonreg'])) {
-	$transfer=$_POST['transfer'];
-	if(empty($transfer)) {
-		echo '<div>Поле не заполнено!</div><hr>';
-	}
+try {
+    $conn = new PDO("sqlsrv:server = tcp:deneg.database.windows.net,1433; Database = asus", "den", "Rosbank20");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "CREATE TABLE Card (
+id INT NOT NULL IDENTITY(1,1)
+PRIMARY KEY (Ncard),
+Ncard varchar(30) NOT NULL,
+Balance INT NOT NULL,
+Phone varchar(30) NOT NULL
+
+)";
+
+$sql1 = "ALTER TABLE Card ADD CONSTRAINT Card_fk0 FOREIGN KEY (id) REFERENCES Klient(id)";
+$conn->query($sql);
+$conn->query($sql1);
+}
+catch (PDOException $e) {
+print("Error connecting to SQL Server.");
+die(print_r($e));
 }
 ?>
-<html>
-	<Title>Номер карты</Title>
-		<legend>Перевод денежных средств</legend>
-<head>
-<title>Background-image</title>
-</head>
-	
-<body Перевод денежных средств>
-<div class="content">
-	<form action="sugnup.php" method="POST">
-	<br Перевод денежных /br>
-		
-		
-	
-		<select name="country">
-<option value="">Выберите страну для перевода</option>
-<option value="Russia">Russia</option>
-<option value="USA">USA</option>
-<option value="Germany">Germany</option>
-<option value="Japan">Japan</option>
-<option value="China">China</option>
-</select>												   
-														   
-		<input name="transfer" type="text" class="input transfer">
-		<select name="country">
-<option value="">Валюта</option>
-<option value="Russia">Рубаль</option>
-<option value="USA">Доллар</option>
-<option value="Europa">Euro</option>
-
-</select>
-	<input type="submit" value="Далее" name="buttonreg">
-		       
-	
-
-	</form>
-    </div>
-
-</body>
-</html>
-
