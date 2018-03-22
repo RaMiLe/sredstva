5ccc178e8176ec1fdad5ce56f48237667391565e
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-22 22:31:19 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 
-<?php
-try {
-    $conn = new PDO("sqlsrv:server = tcp:deneg.database.windows.net,1433; Database = asus", "den", "Rosbank20");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "CREATE TABLE Card (
-id INT NOT NULL IDENTITY(1,1)
-PRIMARY KEY (Ncard),
-Ncard varchar(30) NOT NULL,
-Balance INT NOT NULL,
-Phone varchar(30) NOT NULL
-
-)";
-
-$sql1 = "ALTER TABLE Card ADD CONSTRAINT Card_fk0 FOREIGN KEY (id) REFERENCES Klient(id)";
-$conn->query($sql);
-$conn->query($sql1);
-}
-catch (PDOException $e) {
-print("Error connecting to SQL Server.");
-die(print_r($e));
-}
-?>
+<form action="checkcode.php"...>
+Код: <input type="text" name="code"><br>
+<input type="submit" value="Проверить">
+</form>
