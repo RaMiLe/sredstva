88206d9ee0e8d85fd30230084feca12fb1ad3e00
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-22 22:53:30 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 
-<form action="pol.php"...>
-Код: <input type="text" name="code"><br>
-<input type="submit" value="Проверить">
+<form method="POST" action="https://merchant.webmoney.ru/lmi/payment.asp" target="blank">
+<input name="LMI_PAYMENT_AMOUNT" size="5" value="10" style=" border: 1px solid"> WMZ
+<input type="hidden" name="LMI_PAYMENT_DESC" value="Пополнение счета <? echo $plyrname; ?>">
+<input type="hidden" name="LMI_PAYEE_PURSE" value="<? echo $u_wmz=Z0000000000; ?>">
+<input type="hidden" name="USER" value="<? echo $plyrname; ?>">
+<input type=submit value="Оплатить">
 </form>
