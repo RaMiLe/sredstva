7349f85fa3dd0503b22a90fc7c692b42aea549ad
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-03-29 14:11:41 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,21 +1,21 @@
 <?
 try {
-  $conn = new PDO("sqlsrv:server = tcp:deneg.database.windows.net,1433; Database = asus", "den", "Rosbank20");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $sql = "CREATE TABLE registration_on(
-        id INT NOT NULL IDENTITY(1,1), 
-        PRIMARY KEY(id),
-        name VARCHAR(30),
-        email VARCHAR(30),
-        gender VARCHAR(10),
-        reg_date DATE)";
-        $conn->query($sql);
-        
-        echo "<h3>Таблица создана.</h3>";
-    }
-catch (PDOException $e) {
-    print("Ошибка подключения к SQL Server.");
-    die(print_r($e));
-}
-?>
+      $conn = new PDO("sqlsrv:server = tcp:rom.database.windows.net,1433; Database = qqq", "rom", "Rosbank1997");
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ -$sql = "CREATE TABLE registration_too(
+ +$sql = "CREATE TABLE registration_on(
+  id INT NOT NULL IDENTITY(1,1),
+  PRIMARY KEY(id),
+  name VARCHAR(30),
+ email VARCHAR(30),
+ country VARCHAR(10),
+ date DATE)";
+ $conn->query($sql);
+ 
+ echo "<h3>Таблица создана.</h3>";
+ }
+ catch (PDOException $e) {
+ print("Ошибка подключения к SQL Server.");
+ die(print_r($e));
+ }
+ ?>
